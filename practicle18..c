55797d0aa226190377538eaeaca1042a4be56740
vagdodaritu <vagdodaritu@gmail.com>
2025-11-18 15:13:42 +0530

    practicl18

---
+++ practicle18..c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+    printf("Name:Ritu Vagdoda\nID Number:25CE128\n");
+    int i,j;
+    for(i=1;i<=10;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            printf("%d\t",i+j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
